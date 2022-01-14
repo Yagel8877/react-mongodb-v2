631428de641c75c1ec24c63190e8e1a27b1eea0b
yagel <yagel8877@gmail.com>
2022-01-14 18:02:47 +0200

    2nd.4

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const dbURI = "mongodb+srv://yagel:VDHcur2014@cluster0.gkqyy.mongodb.net/credent
 //     .catch((err) => {console.log(err)})
 
 app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html')).then(console.log('route home'));
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
 
 app.get('/express_backend', (req,res) => {
