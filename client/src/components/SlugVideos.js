5d1f32efe5c7cd4a8a2831a0ebe89f3366a3338d
yagel <yagel8877@gmail.com>
2022-02-10 18:12:58 +0200

    better logic of populating pages

--- client/src/components/SlugVideos.js
+++ client/src/components/SlugVideos.js
@@ -3,40 +3,18 @@ import { useParams } from "react-router";
 
 const SlugVideos = () => {
     let { slug } = useParams();
-    let Nslug = slug - 1;
     let IntSlug = parseInt(slug)
+    let Nslug = IntSlug - 1;
     let Ldata = data.length
     let NLdata = Ldata - 8* Nslug;
-        console.log(NLdata)
-    if(IntSlug === 1){
+    let SlicedData = data.slice(Nslug,Nslug+8)
+    console.log(NLdata)
     return (
-        
         <div className="grid grid-cols-2 bg-black border-[0.5vw] border-red-700 place-items-center justify-center ml-[20vw] mr-[20vw] h-[100vh] gap-2">
-            {data.map((data) => {
-            if(data.vid <= Ldata && data.vid >= Ldata - 7 ){
-                return(    
-                        <div key={data.vid} className='bg-yellow-200 w-[70%] h-[17vh]'>{data.vid}</div>
-                )
-            }
-            else return undefined;
+            {SlicedData.map((data) => {
+                return(<div key={data.vid} className='bg-yellow-200 w-[70%] h-[17vh]'>{data.vid}</div>)
         })}
         </div>
-        
     )
     }
-    else 
-    return(
-        <div className="grid grid-cols-2 bg-black border-[0.5vw] border-red-700 place-items-center justify-center ml-[20vw] mr-[20vw] h-[100vh] gap-2">
-            {data.map((data) => {
-            if(data.vid <= NLdata && data.vid >= NLdata - 7){
-                return(    
-                        <div key={data.vid} className='bg-yellow-200 w-[70%] h-[17vh]'>{data.vid}</div>
-                )
-            }
-            else return undefined;
-        })}
-        </div>
-    )
-    
-}
 export default SlugVideos;
